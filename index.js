c2b70e04eeeec51d6bd3ef726b9b2090772443d7
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-03-13 15:02:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//document.getElementById("count-el").innerText = 5
+
+
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+let countEl = document.getElementById("count-el");
+let saveEn = document.getElementById("entries");
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.innerText = count;
+}
+
+function save() {
+    contentStr = count + " - "
+    saveEn.textContent += contentStr
+    count = 0;
+    countEl.innerText = 0;
+}
